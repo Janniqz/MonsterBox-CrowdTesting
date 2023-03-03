56b95d49d6b42f0e48c436959c686daf60614c0d
Jannik Weise <mail@jannikweise.de>
2023-03-03 15:32:34 +0100

    Added Error Page
    Added global Error Page.
    Displays the Error Code + Message and allows the User to return to the Main Page.

---
+++ src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { page } from "$app/stores"
+</script>
+
+<div id="error-container" class="flex flex-col w-screen h-screen">
+    <div id="error-text-container" class="flex-row grid grid-cols-2 divide-x-4 mt-auto mx-auto mb-5">
+        <div class="px-3 text-right text-7xl font-bold">Error</div>
+        <div class="px-3 text-left text-2xl">
+            <p>{$page.status}</p>
+            <p>{$page.error.message}</p>
+        </div>
+    </div>
+    <a class="w-36 h-8 border-2 rounded-3xl mb-auto mx-auto text-center leading-7 transition ease-in-out bg-transparent text-white hover:bg-white hover:text-black duration-300" href="/">
+        Return
+    </a>
+</div>
