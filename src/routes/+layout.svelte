2086e80b08a453b42f674acbed8731665ddfd082
Jannik Weise <mail@jannikweise.de>
2023-03-03 15:30:33 +0100

    Added Favicons
    Favicons added to +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,18 @@
     import "../app.css";
 </script>
 
+<svelte:head>
+    <!-- FAVICONS -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/imgs/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/imgs/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/imgs/favicons/favicon-16x16.png">
+    <link rel="shortcut icon" href="/imgs/favicons/favicon.ico">
+    <link rel="manifest" href="/imgs/favicons/site.webmanifest">
+    <link rel="mask-icon" href="/imgs/favicons/safari-pinned-tab.svg" color="#000000">
+    <meta name="msapplication-TileColor" content="#000000">
+    <meta name="msapplication-TileImage" content="/imgs/favicons/mstile-150x150.png">
+    <meta name="msapplication-config" content="/imgs/favicons/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+</svelte:head>
+
 <slot />
