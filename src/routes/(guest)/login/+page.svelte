7bb7acab2a62c3d3abc12c214b0f410936188271
Jannik Weise <mail@jannikweise.de>
2023-05-10 22:37:01 +0200

    Updated MonsterBox Logo
    New version of the MonsterBox Text has been added.
    Changes to the "MonsterBox Red" Color have been made to the tailwind.config.
    
    An .SVG for the actual MonsterBox Logo has also been added.

--- src/routes/(guest)/login/+page.svelte
+++ src/routes/(guest)/login/+page.svelte
@@ -44,9 +44,9 @@
 
 <div class="flex w-screen h-screen">
 	<div class="m-auto">
-		<img class="mb-3" src="/imgs/logos/MonsterBox_Text.svg" alt="MonsterBox">
+		<img class="mb-5 w-80" src="/imgs/logos/MonsterBox_Text.svg" alt="MonsterBox">
 		<form class="flex flex-col text-center m-auto" on:submit|preventDefault="{handleLogin}">
-			<input class="bg-gray-800 border border-transparent outline-none rounded py-2 px-3 mb-3 transition focus-visible:border-mb-red hover:border-mb-red duration-300" type="email" placeholder="mail@email.com" bind:value="{email}" />
+			<input class="bg-gray-800 border border-transparent outline-none rounded py-2 px-3 mb-5 transition focus-visible:border-mb-red hover:border-mb-red duration-300" type="email" placeholder="mail@email.com" bind:value="{email}" />
 			<input type="submit"
 						 class="mx-auto w-36 h-8 border-2 outline-none rounded-3xl transition ease-in-out bg-transparent text-white cursor-pointer hover:bg-white focus-visible:bg-white hover:text-black focus-visible:text-black duration-300"
 						 value={buttonText}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'mb-red': '#cf3b74',
+        'mb-red': '#f75663',
       }
     },
   },
