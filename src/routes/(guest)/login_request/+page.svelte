9dfb3e67dd3ca9517c44560c198ece35deb5db0e
Jannik Weise <mail@jannikweise.de>
2023-05-11 11:40:05 +0200

    Login Request Error Handling
    If the Login Request URL (Magic Link) contains the "error" parameter in the Hash field, the User will be redirected back to the Login page with the respective error_description.
    This can happen if the Link has expired or was used multiple times.

--- src/routes/(guest)/login_request/+page.svelte
+++ src/routes/(guest)/login_request/+page.svelte
@@ -1,3 +1,19 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
+	import { addAlert } from '$components/alert/alertStore';
+	import { goto } from '$app/navigation';
+
+	onMount(() => {
+		let params = new URLSearchParams($page.url.hash.substring(1));
+		if (params.has('error')) {
+			addAlert(params.get('error_description')!, false);
+			goto('/login')
+		}
+	})
+
+</script>
+
 <div class="flex w-screen h-screen">
 	<div class="m-auto">
 		<div class="w-12 h-12 rounded-full animate-spin border-4 border-solid border-white border-t-transparent"></div>
